19baf5ab0e6565e3d113d420f2642fcedaa0054e
Stephen Fudge <sfudge@gmail.com>
2023-04-10 17:08:57 -0400

    addex text to about me section

---
+++ src/pages/About/index.js
@@ -0,0 +1,32 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <img src="./images/stephen.png" alt="Stephen Fudge profile pic" className="py-3"/>
+      <p>
+        {" "}
+        Stephen Fudge is a talented and experienced software developer with a
+        Full Stack Web Development Certificate from the University of Toronto.{" "}
+      </p>
+      <p>
+        He has a strong background in technical programming and proficiency in a
+        range of technologies, including HTML, CSS, JavaScript, ReactJS, NodeJS,
+        MongoDB, SQL.
+      </p>
+      <p>
+        Stephen's ability to teach and support others is evident from his
+        experience as an Assistant Instructor with 2U, where he provided
+        guidance and instructional support to up to 40+ students.
+      </p>
+      <p>
+        He is also skilled in managing teams, conducting interviews, and
+        mentoring employees.
+      </p>
+      <p>
+        Stephen's technical expertise, combined with his dedication to
+        delivering exceptional customer service, make him an ideal candidate for
+        a software development role.
+      </p>
+    </div>
+  );
+}
