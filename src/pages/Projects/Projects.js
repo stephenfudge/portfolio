53bc471aa067cd3b239ea1939cc66f41136d486d
Stephen Fudge <sfudge@gmail.com>
2023-04-11 22:14:02 -0400

    added anoter project to json file. edited the way the buttons are showing on projects

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -2,18 +2,18 @@ export default function ProjectCard(props) {
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl py-3">
       <figure>
-        <img src={props.image} alt={props.title} />
+        <img src={props.image} alt={props.title}/>
       </figure>
       <div className="card-body">
         <h2 className="card-title">{props.title}</h2>
         <p>{props.description}</p>
         <p>{props.technologies}</p>
-        <div className="card-actions justify-end">
+        <div className="card-actions">
           <a href={props.repo} target="_blank" rel="noopener noreferrer">
             <button className="btn btn-primary">Repository</button>
           </a>
         </div>
-        <div className="card-actions justify-start">
+        <div className="card-actions">
           <a href={props.deployed} target="_blank" rel="noopener noreferrer">
             <button className="btn btn-primary">Deployed</button>
           </a>
