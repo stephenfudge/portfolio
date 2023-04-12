327d977cf7492526d1faf637e831273ac80df7bc
Stephen Fudge <sfudge@gmail.com>
2023-04-11 21:50:36 -0400

    added images for projects and added pathways to projects.json. updated alt tag in projects.js

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,6 +1,6 @@
 export default function ProjectCard(props) {
   return (
-    <div className="card lg:card-side bg-base-100 shadow-xl">
+    <div className="card lg:card-side bg-base-100 shadow-xl py-3">
       <figure>
         <img src={props.image} alt={props.title} />
       </figure>
@@ -9,12 +9,12 @@ export default function ProjectCard(props) {
         <p>{props.description}</p>
         <p>{props.technologies}</p>
         <div className="card-actions justify-end">
-          <a href={props.repo}>
+          <a href={props.repo} target="_blank" rel="noopener noreferrer">
             <button className="btn btn-primary">Repository</button>
           </a>
         </div>
         <div className="card-actions justify-start">
-          <a href={props.deployed}>
+          <a href={props.deployed} target="_blank" rel="noopener noreferrer">
             <button className="btn btn-primary">Deployed</button>
           </a>
         </div>
