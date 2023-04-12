bbca9a5e1b4dce809c85c7b99ac3da6fb31057ab
Stephen Fudge <sfudge@gmail.com>
2023-04-11 21:19:24 -0400

    created form for contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,80 @@
-export default function Contact(){
-    return(
-        <div>
-        <h1>Contact</h1>
+export default function Contact() {
+  return (
+    <div>
+      <div className="min-h-screen flex flex-col justify-center py-12 px-6 lg:px-8">
+        <h1>Fill out the form below to Contact Me</h1>
+
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-gray-100 py-8 px-6 shadow rounded-lg sm:px-10">
+            <form className="mb-0 space-y-6">
+              {/* Name section required */}
+              <div>
+                <label
+                  for="name"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Name
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="name"
+                    name="name"
+                    type="name"
+                    required
+                    className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+                  />
+                </div>
+              </div>
+              {/* Email section required */}
+              <div>
+                <label
+                  for="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email address
+                </label>
+                <div className="mt-1">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    required
+                    className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+                  />
+                </div>
+              </div>
+              {/* Message section required */}
+              <div>
+                <label
+                  for="message"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Enter a Message:
+                </label>
+                <div className="mt-1">
+                  <textarea
+                    id="message"
+                    name="message"
+                    type="message"
+                    required
+                    className="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-    )
-} 
+      </div>
+    </div>
+  );
+}
