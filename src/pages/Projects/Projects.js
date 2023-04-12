e6f2f65b7d348ab01ba599ae86e96553523a2e5f
Stephen Fudge <sfudge@gmail.com>
2023-04-11 22:34:40 -0400

    created a css file for projects and implemented it

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -2,18 +2,16 @@ export default function ProjectCard(props) {
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl py-3">
       <figure>
-        <img src={props.image} alt={props.title}/>
+        <img src={props.image} alt={props.title} className="project-image"/>
       </figure>
       <div className="card-body">
         <h2 className="card-title">{props.title}</h2>
         <p>{props.description}</p>
         <p>{props.technologies}</p>
-        <div className="card-actions">
+        <div className="card-actions justify-center py-3">
           <a href={props.repo} target="_blank" rel="noopener noreferrer">
             <button className="btn btn-primary">Repository</button>
           </a>
-        </div>
-        <div className="card-actions">
           <a href={props.deployed} target="_blank" rel="noopener noreferrer">
             <button className="btn btn-primary">Deployed</button>
           </a>
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,17 +1,18 @@
 import ProjectCards from "./Projects";
 import projects from "./projects.json";
+import "./projects.css";
 
-export default function Projects(props) {
+export default function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-     {projects.map((project) => {
-       return (
-<div>
-           <ProjectCards key={project.id} {...project} />
-     </div>
-       )
-     })}
+      {projects.map((project) => {
+        return (
+          <div>
+            <ProjectCards key={project.id} {...project} />
+          </div>
+        );
+      })}
     </div>
   );
 }
