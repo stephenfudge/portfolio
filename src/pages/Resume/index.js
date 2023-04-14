77d630e806b1f1f841d49d013e7c84887d166603
Stephen Fudge <sfudge@gmail.com>
2023-04-14 16:44:46 -0400

    added a rough list of skills to resume page

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,7 +1,39 @@
-export default function Resume(){
-    return(
-        <div>
-            <h1>Resume</h1>
-        </div>
-    )
+export default function Resume() {
+  return (
+    <div>
+      <h1 className="text-4xl underline bold">Resume</h1>
+      <h2 className="text-2xl underline bold">Skills</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Bootstrap</li>
+        <li>TailwindCSS</li>
+        <li>Javascript</li>
+        <li>jQuery</li>
+        <li>Handlebars</li>
+        <li>React</li>
+        <li>NextJS</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>Heroku</li>
+        <li>Sequelize</li>
+        <li>MySQL</li>
+        <li>Mongoose</li>
+        <li>MongoDB</li>
+        <li>RESTful APIs</li>
+        <li>GraphQL</li>
+        <li>JSON</li>
+      </ul>
+
+      <a
+        href="https://www.linkedin.com/in/stephenfudge"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="border border-green py-2 px-5">LinkedIn</button>
+      </a>
+    </div>
+  );
 }
