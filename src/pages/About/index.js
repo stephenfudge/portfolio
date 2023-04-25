a0f10bc009f6afe146626fc8015087ea0a1b3d3b
Stephen Fudge <sfudge@gmail.com>
2023-04-25 16:17:02 -0400

    imporved responsiveness for about

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,29 +1,46 @@
 import "./about.css";
+import { useWindowSize } from "react-use";
 
 export default function About() {
+  const { width } = useWindowSize();
+  const imageWidth = width < 768 ? "w-2/3" : "w-1/3";
+
   return (
-    <div className="grid grid-cols-2 gap-2 py-14">
-      <img
-        src="./images/stephen.png"
-        alt="Stephen Fudge profile pic"
-        className="w-full object-cover col-span-1 flex items-center justify-center my-5 rounded-full"
-        id="image"
-      />
+    <div className="grid grid-cols-1 gap-2 py-14 md:grid-cols-2">
+      {width < 768 ? (
+        <div className="flex items-center justify-center">
+          <img
+            src="./images/stephen.png"
+            alt="Stephen Fudge profile pic"
+            className={`object-cover my-5 rounded-full ${imageWidth}`}
+          />
+        </div>
+      ) : (
+        <div className="col-span-1 flex items-center justify-center">
+          <img
+            src="./images/stephen.png"
+            alt="Stephen Fudge profile pic"
+            className={`object-cover col-span-1 my-5 rounded-full ${imageWidth}`}
+          />
+        </div>
+      )}
       <div className="col-span-1 flex items-center justify-center">
-        <p className="text-center text-cyclamen mr-9 pr-9">
-          Hey hey, I'm Stephen Fudge a talented and experienced software
-          developer with a Full Stack Web Development Certificate from the
-          University of Toronto. I have a strong background in technical
-          programming and proficiency in a range of technologies, including
-          HTML, CSS, JavaScript, ReactJS, NodeJS, MongoDB, SQL. My ability to
-          teach and support others is evident from my experience as an Assistant
-          Instructor with 2U, where I provided guidance and instructional
-          support to up to 40+ students. I am also skilled in managing teams,
-          conducting interviews, and mentoring employees. My technical
-          expertise, combined with my dedication to delivering exceptional
-          customer service, make me an ideal candidate for a software
-          development role.
-        </p>
+        <div className="text-center text-cyclamen ">
+          <p className="flex items-center justify-center h-full">
+            Hey hey, I'm Stephen Fudge a talented and experienced software
+            developer with a Full Stack Web Development Certificate from the
+            University of Toronto. I have a strong background in technical
+            programming and proficiency in a range of technologies, including
+            HTML, CSS, JavaScript, ReactJS, NodeJS, MongoDB, SQL. My ability to
+            teach and support others is evident from my experience as an
+            Assistant Instructor with 2U, where I provided guidance and
+            instructional support to up to 40+ students. I am also skilled in
+            managing teams, conducting interviews, and mentoring employees. My
+            technical expertise, combined with my dedication to delivering
+            exceptional customer service, make me an ideal candidate for a
+            software development role.
+          </p>
+        </div>
       </div>
     </div>
   );
