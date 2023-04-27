6048b3d3cf900a367cbc9acd53aa5267e61ec35b
Stephen Fudge <sfudge@gmail.com>
2023-04-27 18:06:52 -0400

    fixed the alt tag to show the name of the project

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -6,7 +6,7 @@ export default function ProjectCard(props) {
                 <div className='item'>
                     <h2 className='project-name text-2xl text-bold underline text-lightpink'><a href={props.deployed} target="_blank" rel="noopener noreferrer">{props.title}</a></h2><br />
                     <h3 className="project-description">{props.description}</h3><br />
-                    <img className='project-images' src={props.image} alt='screenshot of {project.title}'/> <br />
+                    <img className='project-images' src={props.image} alt={`screenshot of ${props.title}`}/> <br />
                     <div className="btn-group btn-group-vertical lg:btn-group-horizontal">
                     <a href={props.deployed} target="_blank" rel="noopener noreferrer"><button className="btn btn-rose mr-2">See it live!</button></a><br />
                     <a href={props.repo} target="_blank" rel="noopener noreferrer"><button className="btn btn-rose">Take a look at the GitHub Repository</button></a>
