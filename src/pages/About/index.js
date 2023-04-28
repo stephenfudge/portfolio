3ba8c7925195c5e973d0437ad17430460b667bca
Stephen Fudge <sfudge@gmail.com>
2023-04-27 20:38:31 -0400

    about text responsiveness issues at ipad mini size resolved

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,6 +6,7 @@ export default function About() {
   const imageWidth = width < 768 ? "w-2/3" : "w-1/3";
 
   return (
+    <div className="container mx-auto px-4 md:px-10">
     <div className="grid grid-cols-1 gap-2 py-14 md:grid-cols-2">
       {width < 768 ? (
         <div className="flex items-center justify-center">
@@ -43,5 +44,6 @@ export default function About() {
         </div>
       </div>
     </div>
+  </div>
   );
 }
