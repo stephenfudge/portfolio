3c4be9d20db84d361ff167dcbc8eb4a8dc0a7c68
Stephen Fudge <sfudge@gmail.com>
2023-04-29 21:49:39 -0400

    added styling to projects index.js

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -4,18 +4,24 @@ import "./projects.css";
 
 export default function Projects() {
   return (
-    <div className="text-cyclamen">
-      <h1 className="text-3xl underline font-bold text-caribbean">Projects</h1>
-      <p className="pb-8 pt-1">Here are some projects that I have been involved in creating.</p>
-      <br />
-      <div className="project-body">
-      {projects.map((project) => {
-        return (
-          <div className="cards"> 
-            <ProjectCards key={project.id} {...project} />
-          </div>
-        );
-      })}
+    <div className="container mx-auto px-4 md:px-10">
+      <div className="text-cyclamen">
+        <h1 className="text-3xl underline font-bold text-caribbean">
+          Projects
+        </h1>
+        <p className="pb-8 pt-1 text-center">
+          Here are some projects that I have been involved in creating.
+        </p>
+        <br />
+        <div className="project-body">
+          {projects.map((project) => {
+            return (
+              <div className="cards" key={project.id}>
+                <ProjectCards {...project} />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
